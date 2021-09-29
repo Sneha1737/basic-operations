6d16baf7fc53c6df1976b31dc42b32dc59b516b1
Sneha1737 <91622759+Sneha1737@users.noreply.github.com>
2021-09-29 22:08:24 +0530

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,19 +1,22 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
 input.onButtonPressed(Button.AB, function () {
-    if (input.lightLevel() > 200) {
-        basic.showLeds(`
-            . # # # .
-            . # . . .
-            . # # # .
-            . . . # .
-            . # # # .
-            `)
-    } else {
-        basic.clearScreen()
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showString("the capital of India is New Delhi")
+    } else if (input.buttonIsPressed(Button.B)) {
+        basic.showString("I live in Kolkata")
     }
-})
-basic.forever(function () {
-	
-})
-basic.forever(function () {
-	
 })
